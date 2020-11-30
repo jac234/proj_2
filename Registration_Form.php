6fa85d5a8ba4930dc06dda2fa53cbcb13bcae50e
Jefferson A Calle <jac234@afsconnect1.njit.edu>
2020-11-29 23:21:36 -0500

    second commit

--- Registration_Form.php
+++ Registration_Form.php
@@ -1,57 +1,75 @@
 <?php
-    echo '<h2>Registration Results</h2>';
-    //the next five lines get the data from the html form
-    $F_Name = filter_input(INPUT_POST, 'firstName');
-    $L_Name = filter_input(INPUT_POST, 'lastName');
-    $B_Day = filter_input(INPUT_POST, 'birthday');
-    $email = filter_input(INPUT_POST, 'email');
-    $pass  = filter_input(INPUT_POST, 'password');
+require('pdo.php');
+echo '<h2>Registration Results</h2>';
+//the next five lines get the data from the html form
+$f_Name = filter_input(INPUT_POST, 'firstName');
+$l_Name = filter_input(INPUT_POST, 'lastName');
+$b_Day = filter_input(INPUT_POST, 'birthday');
+$email = filter_input(INPUT_POST, 'email');
+$pass  = filter_input(INPUT_POST, 'password');
 
-    $conditions_met = 1;
-    //Cant be empty
-    if (strlen($F_Name) == 0) {
-        echo "First Name: $F_Name<br> - should not be left empty.<br><br>";
-        $conditions_met = 0;
-    }
+$conditions_met = 1;
+//Cant be empty
+if (strlen($f_Name) == 0) {
+    echo "First Name: $f_Name<br> - should not be left empty.<br><br>";
+    $conditions_met = 0;
+}
 
-    //Cant be empty
-    if (strlen($L_Name) == 0){
-        echo "Last Name: $L_Name<br> - should not be left empty.<br><br>";
-        $conditions_met = 0;
-    }
+//Cant be empty
+if (strlen($l_Name) == 0){
+    echo "Last Name: $l_Name<br> - should not be left empty.<br><br>";
+    $conditions_met = 0;
+}
 
-    //Cant be empty
-    if (strlen($B_Day) == 0) {
-        echo "Birthday: $B_Day<br> - should not be left empty.<br><br>";
-        $conditions_met = 0;
-    }
+//Cant be empty
+if (strlen($b_Day) == 0) {
+    echo "Birthday: $b_Day<br> - should not be left empty.<br><br>";
+    $conditions_met = 0;
+}
 
-    //Cant be empty & contains @ character
-    if (strlen($email) == 0){
-        echo "Email Address: $email<br> - should not be left empty.<br><br>";
-        $conditions_met = 0;
-    }
-    if (strpos($email, "@") != TRUE){
-        echo "Email Address: $email<br> - should contain '@' symbol.<br><br>";
-        $conditions_met = 0;
-    }
+//Cant be empty & contains @ character
+if (strlen($email) == 0){
+    echo "Email Address: $email<br> - should not be left empty.<br><br>";
+    $conditions_met = 0;
+}
+if (strpos($email, "@") != TRUE){
+    echo "Email Address: $email<br> - should contain '@' symbol.<br><br>";
+    $conditions_met = 0;
+}
 
-    //Cant be empty & at least 8 character
-    if ((strlen($pass) == 0)){
-        echo "Password: $pass<br> - should not be left empty.<br><br>";
-        $conditions_met = 0;
-    }
-    if ((strlen($pass) < 8)){
-        echo "Password: $pass<br> - should be 8 characters or longer.<br><br>";
-        $conditions_met = 0;
-    }
+//Cant be empty & at least 8 character
+if ((strlen($pass) == 0)){
+    echo "Password: $pass<br> - should not be left empty.<br><br>";
+    $conditions_met = 0;
+}
+if ((strlen($pass) < 8)){
+    echo "Password: $pass<br> - should be 8 characters or longer.<br><br>";
+    $conditions_met = 0;
+}
 
 
-    if ($conditions_met == 1){
-        echo "Page is being redirected . . .";
-         header( "refresh: 5, url='display.php'" );
-    }else{
-        echo "Page is being redirected . . .";
-        header("refresh: 1, url='Registration_Form.html'");
-    }
+if ($conditions_met == 1){
+    $query = "INSERT INTO accounts (email, fname, lname, birthday, password) VALUES (:email, :fname, :lname, :birthday, :password)";
+
+// Create PDO statement
+
+    $statement = $db->prepare($query);
+// Bind form values to SQL
+    $statement->bindValue(':email', $email);
+    $statement->bindValue(':fname', $f_Name);
+    $statement->bindValue(':lname', $l_Name);
+    $statement->bindValue(':birthday', $b_Day);
+    $statement->bindValue(':password', $pass);
+
+// Execute the SQL Query
+    $statement->execute();
+
+// Close the DB connection
+    //$statement->closeCursor();
+    echo "Page is being redirected . . .";
+    header( "Location: display.php?f_Name=$f_Name&l_Name=$l_Name" );
+}else{
+    echo "Page is being redirected . . .";
+    header("refresh: 4, url=Registration_Form.html");
+}
 ?>
